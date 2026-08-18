e16564c52b4c56793ca5e19451667ff8b977c402
Halid DUMAN <93721589+dmnhalid@users.noreply.github.com>
2026-08-18 11:39:31 +0300

    program.cs üzerinde diger calisma hazizligi yapildi

--- 01-basics/01-types/Program.cs
+++ 01-basics/01-types/Program.cs
@@ -21,5 +21,6 @@ Console.WriteLine("=== C# Types ===");
 // TODO: Bugün hangi konuyu çalışıyorsan onun Run() metodunu buraya ekle.
 // Örnek:
 //ValueVsReference.Run();
+//BuiltInTypes.Run();
 
 Console.WriteLine("Bitti.");
